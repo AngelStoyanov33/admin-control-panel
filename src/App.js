6b7d4e6364bd8c6d0eaede182ec7cce14dbe9dd0
Angel Stoyanov <aststoyanov@gmail.com>
2022-02-12 11:02:13 +0200

    [Update] Add empty NotFound Component for later & change App header CSS

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import "./App.css";
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Authetication from "./assets/component/Authentication/authenticationComponent";
 import Dashboard from "./assets/component/Dashboard/dashboardComponent";
 import { ProtectedRoute } from "./assets/component/Dashboard/protectedDashbordRoute";
+import NotFound from "./assets/component/NotFound/notFoundComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Authetication} />
-            <ProtectedRoute exact path="/dashboard" component={Dashboard} />
-          </Switch>
-        </Router>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Authetication} />
+          <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+          {/*<Route path="/404" component={NotFound} />
+  <Redirect from="*" to="/404" />*/}
+        </Switch>
+      </Router>
     </div>
   );
 }
