72c90fa852f1018bbdb8569b27ed498c6fe7d4f9
Angel Stoyanov <aststoyanov@gmail.com>
2022-03-31 08:05:06 +0300

    [Update] Add Grafana, Prometheus and K8s panel redirects to sidebar for easier administration

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ import {
 } from "react-router-dom";
 import Authetication from "./assets/component/Authentication/authenticationComponent";
 import Dashboard from "./assets/component/Dashboard/dashboardComponent";
-import { ProtectedRoute } from "./assets/component/Dashboard/protectedDashbordRoute";
+import ProtectedRoute from "./assets/component/Dashboard/protectedDashbordRoute";
 import NotFound from "./assets/component/NotFound/notFoundComponent";
+import Authenticator from "./assets/service/Authentication/authenticationService";
+import OSMComponent from "./assets/component/Dashboard/OSMComponent/osmComponent";
+import VersionControlComponent from "./assets/component/Dashboard/VersionControlComponent/versionControlComponent";
 
 function App() {
   return (
@@ -18,6 +21,40 @@ function App() {
         <Switch>
           <Route exact path="/" component={Authetication} />
           <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+          <ProtectedRoute
+            exact
+            path="/dashboard/grafana"
+            component={() => {
+              window.location.href = process.env.REACT_APP_GRAFANA_URL;
+              return null;
+            }}
+          />
+          <ProtectedRoute
+            exact
+            path="/dashboard/prometheus"
+            component={() => {
+              window.location.href = process.env.REACT_APP_PROMETHEUS_URL;
+              return null;
+            }}
+          />
+          <ProtectedRoute
+            exact
+            path="/dashboard/map"
+            component={OSMComponent}
+          />
+          <ProtectedRoute
+            exact
+            path="/dashboard/git"
+            component={VersionControlComponent}
+          />
+          <ProtectedRoute
+            exact
+            path="/dashboard/k8s"
+            component={() => {
+              window.location.href = process.env.REACT_APP_K8S_PORTAL_URL;
+              return null;
+            }}
+          />
           {/*<Route path="/404" component={NotFound} />
   <Redirect from="*" to="/404" />*/}
         </Switch>
@@ -26,4 +63,4 @@ function App() {
   );
 }
 
-export default App;
+export default Authenticator(App);
--- src/assets/component/Sidebar/sidebarComponentData.js
+++ src/assets/component/Sidebar/sidebarComponentData.js
@@ -2,8 +2,11 @@ import React from "react";
 import * as FaMaterialIcons from "react-icons/fa";
 import * as AiMaterialIcons from "react-icons/ai";
 import * as IoMaterialIcons from "react-icons/io";
+import MonitorHeartIcon from "@mui/icons-material/MonitorHeart";
+import MonitorIcon from "@mui/icons-material/Monitor";
 import MapIcon from "@mui/icons-material/Map";
 import CodeIcon from "@mui/icons-material/Code";
+import VpnLockIcon from "@mui/icons-material/VpnLock";
 
 export const sidebarData = [
   {
@@ -13,9 +16,17 @@ export const sidebarData = [
     className: "nav-text",
   },
   {
-    title: "Dashboard",
-    path: "/dashboard/test",
-    icon: <FaMaterialIcons.FaHome />,
+    title: "Grafana Monitoring",
+    path: "/dashboard/grafana",
+    redirect: process.env.REACT_APP_GRAFANA_URL,
+    icon: <MonitorIcon />,
+    className: "nav-text",
+  },
+  {
+    title: "Prometheus",
+    path: "/dashboard/prometheus",
+    redirect: process.env.REACT_APP_PROMETHEUS_URL,
+    icon: <MonitorHeartIcon />,
     className: "nav-text",
   },
   {
@@ -28,6 +39,13 @@ export const sidebarData = [
     title: "Git",
     path: "/dashboard/git",
     icon: <CodeIcon />,
+    className: "nav-text",
+  },
+  {
+    title: "Kubernetes Portal",
+    path: "/dashboard/k8s",
+    redirect: process.env.REACT_APP_K8S_PORTAL_URL,
+    icon: <VpnLockIcon />,
     className: "nav-text",
   },
 ];
